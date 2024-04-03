9594e670e05384d4c4bc3be880f76e709a6a01ba
Nadiya Shrai <134630278+deagorn@users.noreply.github.com>
2024-04-03 03:51:08 +0300

    delete file

--- src/redux/Contacts/selectors.js
+++
@@ -1,4 +0,0 @@
-// export const selectContacts = state => state.contacts.items
-// export const selectFilter = state => state.contacts.filter
-// export const selectValue = state => state.contacts.value
-
