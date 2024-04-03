838b48bb1c020b357a527ef52d8d0d35aae71236
Nadiya Shrai <134630278+deagorn@users.noreply.github.com>
2024-04-03 03:48:01 +0300

    delete localstore

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store'
+import { store } from './redux/store'
 import './index.css';
-import { PersistGate } from 'redux-persist/integration/react'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
 			<App />
-		</PersistGate>
 	</Provider>
 )
 
